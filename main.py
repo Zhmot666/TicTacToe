ec6992ce46e543de468734d929b9abb94c6ec5ff
Zhmot666 <mailboxforspam@tut.by>
2023-11-26 17:27:58 +0300

    1. Запилил плеер партий из логов в отдельный модуль plyer.py 2. Сделал функцию стирания экрана кроссплатферменной :) теперь должно одинаково работать и под окнами и под никсами 3. Исправил мелкие изъяны

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import datetime
-from os import system as os_sys
+import os
 import random
 import json
+
+
+def clear_windows():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
 def lst_state(pl):
@@ -93,7 +100,7 @@ def computer_hit(pl, c_h, w_s, c_c):
 
 
 def draw_place(sc, pl, player, w_s):
-    os_sys('cls')
+    clear_windows()
     print(f"Общий счет. {player} {sc['P']} ({w_s['P']}) - 3Ton {sc['C']} ({w_s['C']})")
     print('  1 2 3')
     for i in range(3):
@@ -108,68 +115,69 @@ def draw_place(sc, pl, player, w_s):
         print(u' \u2514\u2500\u2534\u2500\u2534\u2500\u2518')
 
 
-score = {'P': 0, 'C': 0}
-os_sys('cls')
-print('Привет! Меня зовут 3Ton, я суперкомпьютер по игре в Крестики Нолики')
-player_name = input('Представься пожалуйста. Как тебя зовут?: ')
-print(f'Привет {player_name}. Расскажу кратко как будет проходить игра. Общие правила ты, надеюсь, знаешь. \n'
-      f'А теперь подробности. Крестики начинают партию. Каждую партию мы меняемся местами. Для того что бы сделать \n'
-      f'ход надо ввести номер клетки. Номер клетки это двухзначное число которое определяется как в "морском бое", \n'
-      f'первая цифра вертикаль, вторая горизонталь, т.е. левая верхняя клетка это 11, а правая нижняя - это 33. \n'
-      f'Извини за такую корявость, графический интерфейс будет в следующей версии когда разработчик изучит Tkinter )))')
-print('Ладно. Хватит разговоров. Проведем жеребьевку кто будет первым играть крестиками.')
-what_symbols = {'P': 'X', 'C': 'O'} if random.choice(['P', 'C']) == 'P' else {'C': 'X', 'P': 'O'}
-print('Я тут подкинул монетку. Первый крестиками играешь Ты') if what_symbols['P'] == 'X' else (
-    print('Я тут подкинул монетку. Первый крестиками играю Я. Верь мне на слово )))'))
-input('Для продолжения нажмите Enter')
-os_sys('cls')
-count_party = 0
-# with open('3Ton.log', 'a+', encoding='utf-8') as log_file:
-#     print('Партия начата ', datetime.datetime.now(), file=log_file)
-loggers = dict()
-loggers['Соперник'] = player_name
-while True:
-    place = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-    who_play = 'P' if what_symbols['P'] == 'X' else 'C'
-    count_party += 1
-    loggers['Партия ' + str(count_party)] = dict()
-    loggers['Партия ' + str(count_party)]['Кто начал'] = who_play
-    hit_list = list()
-    count_hit = 0
+if __name__ == "__main__":
+    score = {'P': 0, 'C': 0}
+    clear_windows()
+    print('Привет! Меня зовут 3Ton, я суперкомпьютер по игре в Крестики Нолики')
+    player_name = input('Представься пожалуйста. Как тебя зовут?: ')
+    print(f'Привет {player_name}. Расскажу кратко как будет проходить игра. Общие правила ты, надеюсь, знаешь. \n'
+          f'А теперь подробности. Крестики начинают партию. Каждую партию мы меняемся местами. Для того что бы \n'
+          f'сделать ход надо ввести номер клетки. Номер клетки это двухзначное число которое определяется как \n'
+          f'в "морском бое", первая цифра вертикаль, вторая горизонталь, т.е. левая верхняя клетка это 11, а \n'
+          f'правая нижняя - это 33. Извини за такую корявость, графический интерфейс будет в следующей версии когда \n'
+          f'разработчик изучит Tkinter )))')
+    print('Ладно. Хватит разговоров. Проведем жеребьевку кто будет первым играть крестиками.')
+    what_symbols = {'P': 'X', 'C': 'O'} if random.choice(['P', 'C']) == 'P' else {'C': 'X', 'P': 'O'}
+    print('Я тут подкинул монетку. Первый крестиками играешь Ты') if what_symbols['P'] == 'X' else (
+        print('Я тут подкинул монетку. Первый крестиками играю Я. Верь мне на слово )))'))
+    input('Для продолжения нажмите Enter')
+    clear_windows()
+    count_party = 0
+    loggers = dict()
+    loggers['Соперник'] = player_name
     while True:
-        count_hit += 1
-        draw_place(score, place, player_name, what_symbols)
-        if who_play == 'P':
-            hit_position = player_hit(place)
-        else:
-            control_computer = True
-            hit_position = computer_hit(place, count_hit, what_symbols, control_computer)
-            if place[(hit_position // 10) - 1][(hit_position % 10) - 1] != ' ':
-                print('Компьютер допустил ошибку в расчетах. Ход будет сгенерирован в случайную пустую клетку. '
-                      'Читайте лог-файл для выяснения обстоятельств.')
-                control_computer = False
-            if not control_computer:
+        place = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        who_play = 'P' if what_symbols['P'] == 'X' else 'C'
+        count_party += 1
+        loggers['Партия ' + str(count_party)] = dict()
+        loggers['Партия ' + str(count_party)]['Кто начал'] = who_play
+        hit_list = list()
+        count_hit = 0
+        while True:
+            count_hit += 1
+            draw_place(score, place, player_name, what_symbols)
+            if who_play == 'P':
+                hit_position = player_hit(place)
+            else:
+                control_computer = True
                 hit_position = computer_hit(place, count_hit, what_symbols, control_computer)
-        hit_list.append(hit_position)
-        place[(hit_position // 10) - 1][(hit_position % 10) - 1] = what_symbols[who_play]
-        # loggers['Ход ' + str(count_hit)] = [who_play, what_symbols[who_play], str(hit_position)]
-        if check_win(place):
-            draw_place(score, place, player_name, what_symbols)
-            winner = player_name if who_play == 'P' else '3Ton'
-            print(f'ПОБЕДА!!! {winner} выиграл этот раунд, ему +1')
-            score[who_play] += 1
-            what_symbols['P'], what_symbols['C'] = what_symbols['C'], what_symbols['P']
+                if place[(hit_position // 10) - 1][(hit_position % 10) - 1] != ' ':
+                    print('Компьютер допустил ошибку в расчетах. Ход будет сгенерирован в случайную пустую клетку. '
+                          'Читайте лог-файл для выяснения обстоятельств.')
+                    control_computer = False
+                if not control_computer:
+                    hit_position = computer_hit(place, count_hit, what_symbols, control_computer)
+            hit_list.append(hit_position)
+            place[(hit_position // 10) - 1][(hit_position % 10) - 1] = what_symbols[who_play]
+            if check_win(place):
+                draw_place(score, place, player_name, what_symbols)
+                winner = player_name if who_play == 'P' else '3Ton'
+                print(f'ПОБЕДА!!! {winner} выиграл этот раунд, ему +1')
+                score[who_play] += 1
+                what_symbols['P'], what_symbols['C'] = what_symbols['C'], what_symbols['P']
+                break
+            if ' ' not in place[0] and ' ' not in place[1] and ' ' not in place[2]:
+                draw_place(score, place, player_name, what_symbols)
+                print('БОЕВАЯ НИЧЬЯ!!! Никому счет не увеличиваем.')
+                what_symbols['P'], what_symbols['C'] = what_symbols['C'], what_symbols['P']
+                break
+            who_play = 'P' if who_play == 'C' else 'C'
+            loggers['Партия ' + str(count_party)]['Ходы'] = hit_list
+        if input('Будем играть еще? (Да(или Enter)/Нет) ') == 'Нет':
+            print('Конец игры!')
             break
-        if ' ' not in place[0] and ' ' not in place[1] and ' ' not in place[2]:
-            draw_place(score, place, player_name, what_symbols)
-            print('БОЕВАЯ НИЧЬЯ!!! Никому счет не увеличиваем.')
-            what_symbols['P'], what_symbols['C'] = what_symbols['C'], what_symbols['P']
-            break
-        who_play = 'P' if who_play == 'C' else 'C'
-        loggers['Партия ' + str(count_party)]['Ходы'] = hit_list
-    if input('Будем играть еще? (Да(или Enter)/Нет) ') == 'Нет':
-        print('Конец игры!')
-        break
-file_name = 'Logs\\' + str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + '.json'
-with open(file_name, 'w', encoding='utf-8') as log_file:
-    json.dump(loggers, log_file, ensure_ascii=False, indent=4)
+    if not os.path.exists('Logs'):
+        os.mkdir('Logs')
+    file_name = 'Logs\\' + str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + '.json'
+    with open(file_name, 'w', encoding='utf-8') as log_file:
+        json.dump(loggers, log_file, ensure_ascii=False, indent=4)
